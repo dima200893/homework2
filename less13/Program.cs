034b926042deb70b462fdaf27ace2fdfbf2611ae
Dmitry Svyatov <dimka201993@gmail.com>
2022-06-27 18:04:43 +0300

    Less13

---
+++ less13/Program.cs
@@ -0,0 +1,29 @@
+﻿Console.WriteLine("vvedite chislo:");
+int a =Convert.ToInt32(Console.ReadLine());
+int number3=a%10;
+if(a>99 && a<1000)
+{
+    Console.WriteLine(number3);
+}
+else
+{
+    if(a<100)
+    {
+        Console.WriteLine("v dannom chisle net 3 cifri");
+    }
+    else
+    {
+    while(a>1000)
+    {
+        a=a/10;
+        number3=a%10;
+    }
+    
+    Console.WriteLine(number3);
+    }
+   
+}
+
+
+
+
