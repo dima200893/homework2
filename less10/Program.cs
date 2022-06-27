04bdc239fea3421c6fe46bd520230373a8e7afd5
Dmitry Svyatov <dimka201993@gmail.com>
2022-06-27 17:27:32 +0300

    Less10

---
+++ less10/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.WriteLine("Vvedite chislo :");
+int a = Convert.ToInt32(Console.ReadLine());
+int number=a%100;
+int findNumber= number /10;
+if(a>99 && a< 1000)
+{
+    Console.WriteLine(findNumber);
+}
+else
+{
+    Console.WriteLine("chislo ne 3x znachnoe, vvedite drugoe");
+}
